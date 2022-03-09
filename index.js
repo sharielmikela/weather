7a84d2cb44bed0537cccbaa4c34fcb0db4a40580
Shariel <99790601+sharielmikela@users.noreply.github.com>
2022-03-09 00:15:43 +0000

    city id

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
     
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+    // takes in the id from the html form, display in // console. Takes in as string.
+        var cityid = String(req.body.cityidInput);
+        console.log(req.body.cityidInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "1976b0748ce87136a400e0bbc93407d8";
+        const url = "api.openweathermap.org/data/2.5/weather?q=" + cityid +  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -31,14 +31,15 @@ app.post("/", function(req, res) {
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+          const cityid = weatherData.cityidid;
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             
             // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h1> The weather is " cityname + weatherDescription + "<h1>");
+            res.write("<h2>The Temperature in " + city + " " + id + " is " + temp + " Degrees Fahrenheit<h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
